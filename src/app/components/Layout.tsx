98fdf22fef851acd4c568a7b8a2dbba66ae1b5c0
zrx <aurelien.loyer@epitech.eu>
2026-03-17 15:01:02 +0400

    feat: update document title and favicon for White Sharks page

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -6,10 +6,26 @@ import { ThemeProvider } from "./ThemeProvider";
 
 export function Layout() {
   const location = useLocation();
+  const isWhitesSharkPage = location.pathname.startsWith("/whites-shark");
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "auto" });
   }, [location.pathname]);
+
+  useEffect(() => {
+    document.title = isWhitesSharkPage ? "Tchouk'Leu X White Sharks" : "Tchouk'Leu";
+
+    const faviconHref = isWhitesSharkPage ? "/images/WhiteSharksLogo.png" : "/images/logo.png";
+    let faviconElement = document.querySelector("link[rel='icon']") as HTMLLinkElement | null;
+
+    if (!faviconElement) {
+      faviconElement = document.createElement("link");
+      faviconElement.rel = "icon";
+      document.head.appendChild(faviconElement);
+    }
+
+    faviconElement.href = faviconHref;
+  }, [isWhitesSharkPage]);
 
   return (
     <ThemeProvider>
